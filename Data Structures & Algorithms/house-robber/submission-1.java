b3ccd860c01e3ee74150082314a423bdd10a390b
ketki308 <jainketki08@gmail.com>
2026-04-23 17:25:23 +0530

    Add: house-robber - submission-1

---
+++ Data Structures & Algorithms/house-robber/submission-1.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int rob(int[] nums) {
+        int prev2=0;
+        int prev1=0;
+
+        for(int i=0;i<nums.length;i++){
+            int curr=Math.max(prev1,prev2+nums[i]);
+            prev2=prev1;
+            prev1=curr;
+        }
+        return prev1;
+    }
+}
