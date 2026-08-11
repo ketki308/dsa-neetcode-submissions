b15ad4e2966dab64961ee29a8c4c523173ddd2c1
Ketki Jain <jainketki08@gmail.com>
2026-08-11 10:44:54 +0530

    Add: is-anagram - submission-14

---
+++ Data Structures & Algorithms/is-anagram/submission-14.java
@@ -0,0 +1,24 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        if(s.length()!=t.length()) return false;
+        Map<Character,Integer> map=new HashMap<>();
+
+        for(int i=0;i<s.length();i++){
+            map.put(s.charAt(i),map.getOrDefault(s.charAt(i),0)+1);
+        }
+
+        for(int i=0;i<t.length();i++){
+            char c=t.charAt(i);
+            if(map.containsKey(c) && map.get(c)>0){
+               
+                   map.put(c,map.getOrDefault(c,0)-1);
+            }
+            if(map.containsKey(c) && map.get(c)==0){
+                                map.remove(c);
+
+            }
+        }
+
+        return map.isEmpty();
+    }
+}
