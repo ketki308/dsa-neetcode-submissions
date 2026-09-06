64794a167058cad777904965c8301f10369cfd67
Ketki <jainketki08@gmail.com>
2026-09-06 15:51:00 +0530

    Add: binary-search - submission-7

---
+++ Data Structures & Algorithms/binary-search/submission-7.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int search(int[] nums, int target) {
+        if(nums.length<=1) return 0;
+        int i=0,j=nums.length-1;
+        while(i<j){
+            int mid=i+(j-i)/2;
+
+            if(nums[mid]==target) {
+                return mid;
+                }
+            else if(nums[mid]<target){
+                i=mid+1;
+            }else{
+                j=mid;
+            }
+        }
+        return -1;
+    }
+}
