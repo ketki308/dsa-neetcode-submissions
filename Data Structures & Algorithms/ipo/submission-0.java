a5d89d0ad610540f9173bebc9e089ef3876afbc5
Ketki <jainketki08@gmail.com>
2026-09-11 21:24:07 +0530

    Add: ipo - submission-0

---
+++ Data Structures & Algorithms/ipo/submission-0.java
@@ -0,0 +1,22 @@
+public class Solution {
+    public int findMaximizedCapital(int k, int w, int[] profits, int[] capital) {
+        PriorityQueue<Integer> minCapital = new PriorityQueue<>((a, b) -> capital[a] - capital[b]);
+        PriorityQueue<Integer> maxProfit = new PriorityQueue<>((a, b) -> profits[b] - profits[a]);
+
+        for (int i = 0; i < capital.length; i++) {
+            minCapital.offer(i);
+        }
+
+        for (int i = 0; i < k; i++) {
+            while (!minCapital.isEmpty() && capital[minCapital.peek()] <= w) {
+                maxProfit.offer(minCapital.poll());
+            }
+            if (maxProfit.isEmpty()) {
+                break;
+            }
+            w += profits[maxProfit.poll()];
+        }
+
+        return w;
+    }
+}
