4cb882ba6091a85e95fafab6e6887d167816f1a0
Ketki <jainketki08@gmail.com>
2026-09-15 08:07:52 +0530

    Add: majority-element - submission-7

---
+++ Data Structures & Algorithms/majority-element/submission-7.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int majorityElement(int[] nums) {
+        int maj=0;
+        int count=0;
+        for(int num:nums){
+            if(count==0){
+                maj=num;
+                count=1;
+            }
+            else if(num==maj){
+                count++;
+            }else{
+                count--;
+            }
+        }
+
+        count=0;
+        for(int num:nums){
+            if(maj==num){
+                count++;
+            }
+        }
+
+        return maj;
+    }
+}
