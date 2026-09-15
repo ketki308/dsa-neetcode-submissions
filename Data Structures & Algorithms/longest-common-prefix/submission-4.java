999328d128111fa7da50680b3e47806dec32d8eb
Ketki <jainketki08@gmail.com>
2026-09-15 07:19:29 +0530

    Add: longest-common-prefix - submission-4

---
+++ Data Structures & Algorithms/longest-common-prefix/submission-4.java
@@ -0,0 +1,16 @@
+class Solution {
+    public String longestCommonPrefix(String[] strs) {
+        if(strs.length==0) return "";
+        String prev=strs[0];
+        for(int i=1;i<strs.length;i++){
+            String c=strs[i];
+            int j=0;
+            while(j<prev.length() && j<c.length() && prev.charAt(j)==c.charAt(j)){
+                j++;
+            }
+            prev=prev.substring(0,j);
+            if(prev.isEmpty()) return "";
+        }
+        return prev;
+    }
+}
